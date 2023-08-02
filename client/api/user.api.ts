5c1701771ff7521e6346717721ab2d1ee67939c6
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-08-02 17:54:43 +0600

    implemented user.api.ts file in frontend

---
+++ client/api/user.api.ts
@@ -0,0 +1,21 @@
+import { IUser } from "@/types/user.types";
+import { axiosInstance } from ".";
+
+export type IAuthResponse = {
+    success: boolean,
+    message: string,
+    user: IUser
+}
+
+export type IAuthData = {
+    email: string,
+    password: string,
+}
+
+export async function authApi<ReturnResponse extends IAuthResponse = IAuthResponse
+    , D extends IAuthData = IAuthData>(url: string, data: D)
+    : Promise<ReturnResponse> {
+
+    const response = await axiosInstance.post<ReturnResponse>(`/users${url}`, data);
+    return response.data;
+}
