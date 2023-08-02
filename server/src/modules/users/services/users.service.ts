02d2bb1a1a31a1eb113fe8480b458db33563cb46
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-08-02 15:53:44 +0600

    implemented createUser with password hashing with bcrypt in usersService

---
+++ server/src/modules/users/services/users.service.ts
@@ -0,0 +1,42 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/database/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { AuthDto } from '../dtos/auth.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>){}
+
+
+    async createUser(data: AuthDto){
+        try {
+            const isUserFound = await this.getUserByEmail(data.email);
+            if(isUserFound){
+                throw new BadRequestException('User already exists')
+            }
+            const saltOrRounds = 10;
+            const password = data.password;
+            const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+            
+            const newUser = new this.userModel({...data, hashedPassword});
+            await newUser.save();
+            return {
+                success: true,
+                message: 'User created successfully'
+            }
+
+
+        } catch (error) {
+            const err = error as Error;            
+            console.log(err.message);
+            throw new BadRequestException(err.message);
+        }
+    }
+   async getUserByEmail(email: string) {
+        return await this.userModel.findOne({email}).exec()
+    }
+}
+
+
--- server/src/modules/users/users.module.ts
+++ server/src/modules/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { UsersController } from './controllers/users.controller';
+import { UsersService } from './services/users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/database/user.schema';
 
