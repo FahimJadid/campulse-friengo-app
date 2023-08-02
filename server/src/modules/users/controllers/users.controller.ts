e9cf9ee63c198bd42ec0e5fed790c5b97bebb9c7
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-08-02 15:59:38 +0600

    update the users.controller with POST signup

--- server/src/modules/users/controllers/users.controller.ts
+++ server/src/modules/users/controllers/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './../services/users.service';
+import { AuthDto } from '../dtos/auth.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private usersService: UsersService){}
+
+    @Post('signup')
+    async signupUser(@Body() authDto: AuthDto){
+        return await this.usersService.createUser(authDto);
+    }
+}
